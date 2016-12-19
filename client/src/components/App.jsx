789e217c51fd1ea5d68ee384bce33a35c44239f4
ZolayvarE <ZolayvarECU@gmail.com>
2016-12-19 14:30:38 -0800

    Redeploying...

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { Router, Route, Link, browserHistory } from 'react-router';
 import NavBar from './Navbar.jsx';
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: 'I am a stateful component!'
+      message: 'I am a stateful component, yo!'
     };
   }
 
